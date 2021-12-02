adc0bb87a5fa0721e19fec1ba61a46f6d6904412
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-12-02 17:39:18 +0000

    Update test_lipid.py

--- rocklinc/tests/test_lipid.py
+++ rocklinc/tests/test_lipid.py
@@ -27,7 +27,7 @@ class TestCurveLipid():
     @staticmethod
     @pytest.fixture(scope='class')
     def lipid():
-        lipid = CurveLipid([80, 80, 80], [257, 257, 257])
+        lipid = CurveLipid([80, 80, 80], [49, 49, 49])
         lipid.write_dx_input([24.1998191, 1.28409182, 3.29937546e-03,
                               18.5969598, 2.41286946, 5.70594706e-03,
                               16.0770291, 2.47034776, 4.17737240e-03,
@@ -36,7 +36,7 @@ class TestCurveLipid():
                               [0.36626031, 18.87278382, 76.49060424, 4.83151723])
 
         lipid.write_apbs_input()
-        lipid.run_APBS(apbs_exe='/Users/grte2001/opt/miniconda3/bin/apbs')
+        lipid.run_APBS(apbs_exe='apbs')
         charge, diel, esp = lipid.read_APBS()
         fig = plot_dx(np.linspace(0, lipid.dim[-1].magnitude, lipid.grid[-1]), charge, diel, esp)
         return lipid, fig
