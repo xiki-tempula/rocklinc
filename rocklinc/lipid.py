1e44fb3c887a4fe1c3d01d2cd9deb78304473086
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-12-02 15:36:04 +0000

    Update lipid.py

--- rocklinc/lipid.py
+++ rocklinc/lipid.py
@@ -4,6 +4,7 @@ from gridData import Grid
 from pkg_resources import resource_filename
 import shutil
 from subprocess import call
+import matplotlib.pyplot as plt
 
 def gauss(x, mu, sigma, A):
     return A * np.exp(-(x - mu) ** 2 / 2 / sigma ** 2)
@@ -246,7 +247,7 @@ class LipdBase():
         return out_ESP
 
 def StepLipid(LipdBase):
-    @staticmethod
+
     def gaussian_mixture(x, magnitude, neg_loc, neg_width, pos_loc, pos_width):
         ''' Charge density with respect to the distance to the end of the
         lipid.
@@ -281,7 +282,6 @@ def StepLipid(LipdBase):
                      pq.Quantity(pos_width, pq.angstrom),
                      pq.Quantity(magnitude, pq.e / pq.angstrom ** 3))
 
-    @staticmethod
     def diel_model(x, thickness, diel_low, diel_high):
         ''' The dielectric constant with respect to the distance to the end
         of the lipid.
@@ -405,3 +405,52 @@ def CurveLipid(LipdBase):
             The one-dimensional array of the dielectric constant.
         '''
         return k2 / (1 + np.exp(-(k1*(x-b1)))) + b2
+
+def plot_dx(x, charge, diel, lipid):
+    fig, host = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+
+    par1 = host.twinx()
+    par2 = host.twinx()
+
+    # Offset the right spine of par2.  The ticks and label have already been
+    # placed on the right by twinx above.
+    par2.spines["right"].set_position(("axes", 1.2))
+    # Having been created by twinx, par2 has its frame off, so the line of its
+    # detached spine is invisible.  First, activate the frame but make the patch
+    # and spines invisible.
+    par2.set_frame_on(True)
+    par2.patch.set_visible(False)
+    for sp in par2.spines.values():
+        sp.set_visible(False)
+    # Second, show the right spine.
+    par2.spines["right"].set_visible(True)
+
+    size = charge.shape
+    p1, = host.plot(x, [0, 1, 2], "b-", label="Density")
+    p2, = par1.plot([0, 1, 2], [0, 3, 2], "r-", label="Temperature")
+    p3, = par2.plot([0, 1, 2], [50, 30, 15], "g-", label="Velocity")
+
+    host.set_xlim(0, 2)
+    host.set_ylim(0, 2)
+    par1.set_ylim(0, 4)
+    par2.set_ylim(1, 65)
+
+    host.set_xlabel("Distance")
+    host.set_ylabel("Density")
+    par1.set_ylabel("Temperature")
+    par2.set_ylabel("Velocity")
+
+    host.yaxis.label.set_color(p1.get_color())
+    par1.yaxis.label.set_color(p2.get_color())
+    par2.yaxis.label.set_color(p3.get_color())
+
+    tkw = dict(size=4, width=1.5)
+    host.tick_params(axis='y', colors=p1.get_color(), **tkw)
+    par1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+    par2.tick_params(axis='y', colors=p3.get_color(), **tkw)
+    host.tick_params(axis='x', **tkw)
+
+    lines = [p1, p2, p3]
+
+    host.legend(lines, [l.get_label() for l in lines])
